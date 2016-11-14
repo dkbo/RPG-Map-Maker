494b85c46881e22a964d4a6344b63f74042c6827
DKBO <dk880842@gmail.com>
2016-11-14 18:09:35 +0800

    update

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,5 +1,5 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const isProduction = () => process.env.NODE_ENV === 'production';
+const isProduction = () => process.argv.indexOf('-p') !== -1;
 
 module.exports = [
 	{
@@ -13,7 +13,7 @@ module.exports = [
 	},
 	{ test: /\.jade$/, loader: 'jade' },
 	// { test: /\.s?a?c?ss$/, loader: 'style!css!sass' },
-	{ test: /\.s?a?c?ss$/, loader: isProduction() ? ExtractTextPlugin.extract('style!css!postcss!sass') : 'style!css!postcss!sass' },
+	{ test: /\.s?a?c?ss$/, loader: isProduction() ? ExtractTextPlugin.extract('style','css!postcss!sass') : 'style!css!postcss!sass' },
 	{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
 	{ test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000" },
 	{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
